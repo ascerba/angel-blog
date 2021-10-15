123b514f442ff3285e80edeffe8dad8dc0592d11
ascerba <a.scerba02@gmail.com>
2021-10-14 22:59:23 -0400

    Removed old method for sticky and mobile nav

--- nav.js
+++ nav.js
@@ -1,28 +1,4 @@
-function navToggle() {
-  document.getElementById("dropdown").classList.toggle("show");
+function actionToggle() {
+  var action = document.querySelector('.action');
+  action.classList.toggle('active')
 }
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown_button')) {
-    var dropdowns = document.getElementsByClassName("dropdown_content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-function scrollFunction() {
-  var declaration = document.getElementById("sticky_nav").style;
-  var content = document.getElementById("content");
-  if (content.clientWidth > 733 && (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)) {
-    declaration.display = "inline-block";
-  } else {
-    declaration.display = "none";
-  }
-}
-window.onscroll = function() {
-  scrollFunction()
-};
